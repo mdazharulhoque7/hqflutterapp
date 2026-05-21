7a43fae09ccda38b3dd339a4dc601032ef30596a
Md Azharul Hoque <mdazharulhoque7@gmail.com>
2026-05-22 01:39:54 +0600

    create custom widgets for gradient container and appbar

--- lib/main.dart
+++ lib/main.dart
@@ -19,38 +19,16 @@ class MyApp extends StatelessWidget {
     // In this course, you will, of course, not just use them a lot but
     // also learn about many other widgets!
     return MaterialApp(
-      title: 'Flutter First App',
+      title: 'HQ Flutter App',
       theme: ThemeData(useMaterial3: true),
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter', style: TextStyle(color: Colors.white)),
-          flexibleSpace: Container(
-            decoration: BoxDecoration(
-              gradient: const LinearGradient(
-                colors: [Colors.blue, Color.fromARGB(255, 17, 68, 156)],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: const Color.fromARGB(255, 21, 21, 21).withValues(alpha: 0.3),
-                  blurRadius: 10,
-                  offset: const Offset(0, 5),
-                )
-              ] 
-            ),
+        appBar: HQAppBar(
+          title: const Text(
+            'Welcome to Flutter',
+            style: TextStyle(color: Colors.white),
           ),
         ),
-        body: Container(
-          width: double.infinity,
-          padding: const EdgeInsets.all(12),
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.blue, Colors.purple],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
+        body: GradientContainer(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
@@ -68,15 +46,71 @@ class MyApp extends StatelessWidget {
               Text(
                 'Learn Flutter step-by-step, from the ground up.',
                 textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.white70,
-                ), 
+                style: TextStyle(fontSize: 16, color: Colors.white70),
               ),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class HQAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final Widget title;
+
+  const HQAppBar({super.key, required this.title});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: title,
+      // The flexibleSpace property allows you to put a widget behind the AppBar content
+      flexibleSpace: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: <Color>[Color.fromARGB(255, 6, 49, 8), Color.fromARGB(255, 6, 71, 18)],
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromARGB(185, 27, 27, 27),
+              blurRadius: 4,
+              offset: Offset(0, 2),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class GradientContainer extends StatelessWidget {
+  final Widget child;
+
+  const GradientContainer({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(12),
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 18, 70, 20),
+            Color.fromARGB(179, 218, 11, 11),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          stops: [0.7, 1.0],
+        ),
+      ),
+      child: child,
     );
   }
 }
